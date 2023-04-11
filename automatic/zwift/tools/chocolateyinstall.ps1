957f43bb7f203003862fdd98b74f65c6bdd678eb
Nicolas ROGIER <nicolas.rogier@univ-lorraine.fr>
2023-04-11 08:57:13 +0200

    adding zwift package

---
+++ automatic/zwift/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+$url = 'https://cdn.zwift.com/app/ZwiftSetup.exe'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'EXE'
+  url            = $url
+  softwareName   = 'zwift*'
+  checksum       = 'E9EB34A72405D2AA21B38E8A22E444396AAD79184397F14EE1BCAE3BE8E59B57'
+  checksumType   = 'sha256'
+  silentArgs     = '/SP- /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /NOCANCEL'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
