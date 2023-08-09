d6971930b7fe4a213c85c4234746a132771de95f
overag3 <54666189+overag3@users.noreply.github.com>
2023-08-09 11:06:19 +0200

    Update package args for MSI installer

--- automatic/rancher-desktop/tools/chocolateyinstall.ps1
+++ automatic/rancher-desktop/tools/chocolateyinstall.ps1
@@ -3,13 +3,13 @@ $url = 'https://github.com/rancher-sandbox/rancher-desktop/releases/download/v1.
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  fileType       = 'EXE'
+  fileType       = 'MSI'
   url            = $url
   softwareName   = 'Rancher Desktop*'
   checksum       = '5f7bc72fc159864b84464f6dc967be5c7379529b04c983a2d5e1f85b6ff3f358'
   checksumType   = 'sha256'
-  silentArgs     = '/S'
-  validExitCodes = @(0)
+  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes = @(0, 3010, 1641)
 }
 
 Install-ChocolateyPackage @packageArgs
