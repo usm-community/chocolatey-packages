29350c97dfce0e6b4ecd70606c98d0ef52c6dca1
Ayt3ck <timothee.fourel@univ-lorraine.fr>
2024-01-11 10:59:32 +0100

    Upgrade 1.5 => 1.7

--- manual/glpi-agent/tools/chocolateyinstall.ps1
+++ manual/glpi-agent/tools/chocolateyinstall.ps1
@@ -1,15 +1,16 @@
 $ErrorActionPreference = 'Stop';
+$version = '1.7'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'msi'
-  url           = 'https://github.com/glpi-project/glpi-agent/releases/download/1.5/GLPI-Agent-1.5-x86.msi'
-  checksum      = 'b9ffaef446ac86fad22c19d7daf57b76918760ac828d990f390066ec2972d025'
+  url           = "https://github.com/glpi-project/glpi-agent/releases/download/$version/GLPI-Agent-$version-x86.msi"
+  checksum      = 'be498f48350c091de1d5b70dc203b5e01b0b9e0eaa7923601ea0ddd8d333dd33'
   checksumType  = 'sha256' 
-  url64         = 'https://github.com/glpi-project/glpi-agent/releases/download/1.5/GLPI-Agent-1.5-x64.msi'
-  checksum64    = '0f2cfadee623013028b73bf31c89ca3f43a3797c95a7cf45fdeb320ea73a4b7d'
+  url64         = "https://github.com/glpi-project/glpi-agent/releases/download/$version/GLPI-Agent-$version-x64.msi"
+  checksum64    = 'bd1118df888113546499eb9f70d49db4db51ee5e3fdda1ec4c326d4a6e2d6c91'
   checksumType64= 'sha256' 
-  silentArgs    = "/quiet"
+  silentArgs    = '/quiet'
   validExitCodes= @(0, 3010, 1641)
 }
 
