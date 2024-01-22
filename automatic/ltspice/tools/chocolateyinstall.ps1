e8301a20643265fc69ce41aa70e6d9735c02d52f
Nicolas ROGIER <nicolas.rogier@univ-lorraine.fr>
2024-01-22 09:41:37 +0100

    feat: adding ltspice to automatic update

--- automatic/ltspice/tools/chocolateyinstall.ps1
+++ automatic/ltspice/tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
-﻿$ErrorActionPreference = 'Stop';
-$url64 = 'https://ltspice.analog.com/software/LTspice64.msi' 
+﻿$ErrorActionPreference = 'Stop'
+$url64 = 'https://ltspice.analog.com/software/LTspice64.msi'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
@@ -7,18 +7,18 @@ $packageArgs = @{
   url64          = $url64
   softwareName   = 'LTspice*'
   checksum64     = '6718ABBD4F4FAEFE375C579DE775151525A9A1CCA821041796588A6EC05A5C99'
-  checksumType64 = 'sha256' 
-  silentArgs     = "AI_INSTALLPERUSER=0 /quiet"
+  checksumType64 = 'sha256'
+  silentArgs     = 'AI_INSTALLPERUSER=0 /quiet'
 }
 
 Install-ChocolateyPackage @packageArgs
 
 #Disable auto update pop up message
-$UpdateConfigFile = Join-Path $env:ProgramFiles "ADI\LTspice\updater.ini"
-if(Test-Path $UpdateConfigFile){
+$UpdateConfigFile = Join-Path $env:ProgramFiles 'ADI\LTspice\updater.ini'
+if (Test-Path $UpdateConfigFile) {
   $UpdateConfigFileContent = Get-Content $UpdateConfigFile
-  if($UpdateConfigFileContent -match "CheckFrequency=\d"){
-    $UpdatedConfigFileContent = $UpdateConfigFileContent -replace "CheckFrequency=\d","CheckFrequency=0"
+  if ($UpdateConfigFileContent -match 'CheckFrequency=\d') {
+    $UpdatedConfigFileContent = $UpdateConfigFileContent -replace 'CheckFrequency=\d', 'CheckFrequency=0'
     Set-Content -Path $UpdateConfigFile -Value $UpdatedConfigFileContent
   }
 }
---
+++ automatic/ltspice/update.ps1
@@ -0,0 +1,38 @@
+Import-Module AU
+Import-Module ([System.IO.Path]::Combine($env:ChocolateyInstall, 'helpers', 'chocolateyInstaller.psm1'))
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyinstall.ps1' = @{
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function GetResultInformation([string]$url64) {
+    $dest = Join-Path $env:TEMP 'LTspice64.msi'
+    Get-WebFile $url64 $dest | Out-Null
+
+    $checksumType = 'sha256'
+    $result = @{
+        URL64      = $url64
+        Version    = (Get-MsiInformation -Path $dest).ProductVersion
+        Checksum64 = Get-FileHash $dest -Algorithm $checksumType | ForEach-Object Hash
+    }
+    Remove-Item -Force $dest -ErrorAction SilentlyContinue
+
+    return $result
+}
+
+function global:au_GetLatest {
+    $url64 = 'https://ltspice.analog.com/software/LTspice64.msi'
+
+    $result = Update-OnETagChanged -execUrl $url64 -OnETagChanged {
+        GetResultInformation $url64
+    } -OnUpdated { @{ URL64 = $url64 } }
+
+    return $result
+}
+
+update -ChecksumFor none
