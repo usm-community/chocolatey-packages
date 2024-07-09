d993518ab08861bc6d0fcc24505e83083f6820b3
Ayt3ck <timothee.fourel@orange.fr>
2024-07-09 15:43:54 +0200

    Upgrade 1.9 => 1.10

--- manual/glpi-agent/tools/chocolateyinstall.ps1
+++ manual/glpi-agent/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 $ErrorActionPreference = 'Stop';
-$version = '1.9'
+$version = '1.10'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'msi'
   url64         = "https://github.com/glpi-project/glpi-agent/releases/download/$version/GLPI-Agent-$version-x64.msi"
-  checksum64    = 'db0aad29a3b826a1ff7fc65e5d6aec1fd4257d438369c72030d5ac32af3f3b4a'
+  checksum64    = '2ea2300a2e79c5927e283f111d5d1e5ff47e132f1660a6b41dab49933ffd09ae'
   checksumType64= 'sha256' 
   silentArgs    = '/quiet'
   validExitCodes= @(0, 3010, 1641)
