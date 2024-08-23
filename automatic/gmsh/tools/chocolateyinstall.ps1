133a0062a98d9df32e0d8b976791c86990c07c94
ROGIER Nicolas <nicolas.rogier@univ-lorraine.fr>
2024-08-23 17:17:39 +0200

    AU: gmsh 4.13.1

--- automatic/gmsh/tools/chocolateyinstall.ps1
+++ automatic/gmsh/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop'
-$url64 = 'https://gmsh.info/bin/Windows/gmsh-4.12.1-Windows64.zip'
+$url64 = 'https://gmsh.info/bin/Windows/gmsh-4.13.1-Windows64.zip'
 $installDir = 'C:\gmsh'
-$version = '4.12.1'
+$version = '4.13.1'
 $dirToExtract = "gmsh-$version-Windows64"
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $installDir
   url64bit       = $url64
-  checksum64     = '283e2357d24452d037288447493b1aeda2175470a859f31b0233aac702bd3d25'
+  checksum64     = '2a98173869aa2bab77b19be361cfa371f6e4e826535e6386768c1b34ba30cd2e'
   checksumType64 = 'sha256'
 }
 
