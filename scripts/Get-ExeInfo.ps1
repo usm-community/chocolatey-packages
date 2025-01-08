222f657441fd60edafc624b8bf7bbb0bbeeb2e2b
Nicolas Rogier <nicolas.rogier@univ-lorraine.fr>
2025-01-08 16:41:21 +0100

    feat: init repo for auto update process

---
+++ scripts/Get-ExeInfo.ps1
@@ -0,0 +1,27 @@
+﻿function Get-ExeInfo {
+  param(
+      [parameter(Mandatory = $true,
+          HelpMessage = 'Provide the path to an EXE')]
+      [ValidateNotNullOrEmpty()]
+      [System.IO.FileInfo]$Path
+  )
+
+  if (Test-Path $Path) {
+      if (Get-Command 'exiftool' -ErrorAction SilentlyContinue) {
+          $output = exiftool $Path
+
+          $hash = @{}
+          foreach ($line in $output) {
+              $IndexColon = $line.IndexOf(':')
+              $Property = $line.Substring(0, $IndexColon).Trim()
+              $hash[$Property] = ($line.Substring($IndexColon + 1, $line.Length - $IndexColon - 1)).Trim()
+          }
+          return $hash
+          else {
+              throw 'Unable to find exiftool command.'
+          }
+      } else {
+          throw 'Unable to find file.'
+      }
+  }
+}
---
+++ scripts/Get-MsiInfo.ps1
@@ -0,0 +1,29 @@
+﻿function Get-MsiInfo {
+  param(
+      [parameter(Mandatory = $true,
+          HelpMessage = 'Provide the path to an MSI')]
+      [ValidateNotNullOrEmpty()]
+      [System.IO.FileInfo]$Path
+  )
+
+  if (Test-Path $Path) {
+      if (Get-Command 'msiinfo' -ErrorAction SilentlyContinue) {
+          $output = msiinfo export $Path Property
+
+          $hash = @{}
+          $regex = '^(UpgradeCode|ProductCode|ProductLanguage|ProductName|ProductVersion|Manufacturer)'
+          foreach ($line in $output) {
+              if ($line -match $regex) {
+                  $SplitLine = $line.Split("`t")
+                  $hash[$SplitLine[0]] = $SplitLine[1]
+              }
+          }
+          return $hash
+          else {
+              throw 'Unable to find msiinfo command.'
+          }
+      } else {
+          throw 'Unable to find file.'
+      }
+  }
+}
