a5d2a215c2fbfc84f40c90e23495bb1b9b96e9e9
Nicolas Rogier <nicolas.rogier@univ-lorraine.fr>
2025-01-09 11:33:48 +0100

    fix: error insecure redirection

--- automatic/purplepen/update.ps1
+++ automatic/purplepen/update.ps1
@@ -2,7 +2,7 @@ param([string]$IncludeStream, [switch]$Force)
 
 Import-Module au
 
-$releases = 'https://purplepen.golde.org/downloads'
+$releases = 'https://purple-pen.org/downloads'
 
 function global:au_SearchReplace {
     @{
@@ -20,7 +20,7 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
     $regex = 'purplepen-(\d)(\d)(\d)\.exe$'
 
-    $url = ($download_page.Links | Where-Object href -Match $regex | Select-Object -Last 1 -ExpandProperty href )
+    $url = ($download_page.Links | Where-Object href -Match $regex | Select-Object -Last 1 -ExpandProperty href)
     $version = "$($Matches[1]).$($Matches[2]).$($Matches[3])"
 
     return @{
