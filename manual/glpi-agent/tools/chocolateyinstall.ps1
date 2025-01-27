c3992675ef85c3dbceedddd12e2239191ddc3831
Ayt3ck <timothee.fourel@orange.fr>
2025-01-27 12:22:04 +0100

    Upgrade 1.11 => 1.12

--- manual/glpi-agent/tools/chocolateyinstall.ps1
+++ manual/glpi-agent/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 $ErrorActionPreference = 'Stop';
-$version = '1.11'
+$version = '1.12'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'msi'
   url64         = "https://github.com/glpi-project/glpi-agent/releases/download/$version/GLPI-Agent-$version-x64.msi"
-  checksum64    = '9c28e6ea14c1f6aa20eb5beb3a04f3ad4104c763caa58ceb39c076bfc613cd40'
+  checksum64    = 'ab2ec1f9b3ab69d0d3f6ed45742374183caa4b3198d78e21a55d4d4ccd424e19'
   checksumType64= 'sha256' 
   silentArgs    = '/quiet'
   validExitCodes= @(0, 3010, 1641)
