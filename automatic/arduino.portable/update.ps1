b5670be6c55a33f707ad43dd24bd5193d52f585d
ROGIER Nicolas <nicolas.rogier@univ-lorraine.fr>
2025-09-02 07:12:26 +0000

    fix: AU arduino.portable

--- automatic/arduino.portable/update.ps1
+++ automatic/arduino.portable/update.ps1
@@ -1,10 +1,11 @@
-Import-Module au
+Import-Module AU
 
 $releases = 'https://www.arduino.cc/en/software'
+$releases = 'https://api.github.com/repos/arduino/arduino-ide/releases/latest'
 
 function global:au_SearchReplace {
     @{
-        ".\tools\chocolateyInstall.ps1" = @{
+        ".\tools\chocolateyinstall.ps1" = @{
             "(^[$]version\s*=\s*)('.*')"  = "`$1'$($Latest.Version)'"
             "(^[$]url64\s*=\s*)('.*')"    = "`$1'$($Latest.URL64)'"
             "(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
@@ -13,9 +14,10 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $url = $download_page.links | Where-Object href -match 'arduino-ide_(?<version>[\d\.]+)_Windows_64bit.zip$' | Select-Object -First 1 -ExpandProperty href
-    $version = $Matches.version
+    $latest_asset = (Invoke-RestMethod -Uri $releases).assets
+    $url = $latest_asset | Where-Object browser_download_url -Match 'arduino-ide_(?<version>[\d\.]+)_Windows_64bit.zip$' | Select-Object -First 1 -ExpandProperty browser_download_url
+
+    $version = Get-Version $url
 
     return @{
         Version  = $version
