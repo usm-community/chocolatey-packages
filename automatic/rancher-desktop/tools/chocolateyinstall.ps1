5b4feb9b5035fbb81758a9c9e20027bd03143969
overag3 <nicolas.rogier@univ-lorraine.fr>
2025-09-03 14:14:12 +0000

    AU: 4 updated - geogebra-classic.install gmsh gretl rancher-desktop [skip ci]

--- automatic/rancher-desktop/tools/chocolateyinstall.ps1
+++ automatic/rancher-desktop/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop'
-$url = 'https://github.com/rancher-sandbox/rancher-desktop/releases/download/v1.18.2/Rancher.Desktop.Setup.1.18.2.msi'
+$url = 'https://github.com/rancher-sandbox/rancher-desktop/releases/download/v1.20.0/Rancher.Desktop.Setup.1.20.0.msi'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'MSI'
   url            = $url
   softwareName   = 'Rancher Desktop*'
-  checksum       = 'F5824A37FBAB9A46A4FC062E1F16DCF09CDC799D4DADF04088650309CCA4ADBF'
+  checksum       = '1d299e97247bbed740698017a9146eec3f0cb5b42c9a57f0a4e7851076dd3493'
   checksumType   = 'sha256'
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0, 3010, 1641)
