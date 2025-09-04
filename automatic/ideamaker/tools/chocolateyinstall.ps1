45c79f12b9e80a93f8fa7ada1d5e41a1db7f9823
ROGIER Nicolas <nicolas.rogier@univ-lorraine.fr>
2025-09-04 10:49:36 +0200

    AU: ideamaker 5.2.4.8581

--- automatic/ideamaker/tools/chocolateyinstall.ps1
+++ automatic/ideamaker/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop'
-$url = 'https://downcdn.raise3d.com/ideamaker/release/5.2.3/install_ideaMaker_5.2.3.8580.exe'
+$url = 'https://downcdn.raise3d.com/ideamaker/release/5.2.4/install_ideaMaker_5.2.4.8581.exe'
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     fileType       = 'EXE'
     url            = $url
     softwareName   = 'ideamaker*'
-    checksum       = 'dbfac47f657cbbf536c41fcc8e46951bf76b28e4e79da564c936a21ea7eb460e'
+    checksum       = '3259caa541e63672804b72bc9a6ce417853ad015903b4a33b55f306cd4602434'
     checksumType   = 'sha256'
     silentArgs     = '/S'
     validExitCodes = @(0)
