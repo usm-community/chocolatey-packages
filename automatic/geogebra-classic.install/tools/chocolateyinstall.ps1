452e664dad6eac48d73f11816ab02f85f5192740
overag3 <nicolas.rogier@univ-lorraine.fr>
2025-09-09 00:57:28 +0000

    AU: 1 updated - geogebra-classic.install [skip ci]

--- automatic/geogebra-classic.install/tools/chocolateyinstall.ps1
+++ automatic/geogebra-classic.install/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'
-$url = 'https://download.geogebra.org/installers/6.0/GeoGebra-Windows-Installer-6-0-898-1.msi'
-$version = [version]'6.0.898.1'
+$url = 'https://download.geogebra.org/installers/6.0/GeoGebra-Windows-Installer-6-0-899-0.msi'
+$version = [version]'6.0.899.0'
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     fileType       = 'MSI'
     url            = $url
     softwareName   = 'GeoGebra Classic*'
-    checksum       = 'bbc1079131e8d05fe2eca1e39f6e4ab9ca594c3f58d9b20d6a42aad8dc12d374'
+    checksum       = 'bd7988ef797de928308d84271123a51140fc57b6aa943b53bf49284db2df3420'
     checksumType   = 'sha256'
     silentArgs     = "ALLUSERS=2 /qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
     validExitCodes = @(0, 3010, 1641)
