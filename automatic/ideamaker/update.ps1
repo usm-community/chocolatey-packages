e8ba9fb4875f5c3a0ff5fa25358a571e07c435d4
Nicolas Rogier <nicolas.rogier@univ-lorraine.fr>
2025-09-12 09:14:25 +0200

    fix: delete header Connection

--- automatic/ideamaker/update.ps1
+++ automatic/ideamaker/update.ps1
@@ -23,7 +23,6 @@ function global:au_GetLatest {
         "Accept-Language"           = "en-US,en;q=0.5"
         "Accept-Encoding"           = "gzip, deflate"
         "DNT"                       = "1"
-        "Connection"                = "keep-alive"
         "Upgrade-Insecure-Requests" = "1"
     }
 
@@ -39,7 +38,6 @@ function global:au_GetLatest {
                 "Accept-Language"           = "en-US,en;q=0.5"
                 "Accept-Encoding"           = "gzip, deflate"
                 "DNT"                       = "1"
-                "Connection"                = "keep-alive"
                 "Upgrade-Insecure-Requests" = "1"
                 "User-Agent"                = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
             }
