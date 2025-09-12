53fed18a2513513f7041c3c6005bb88d1be4f854
Nicolas Rogier <nicolas.rogier@univ-lorraine.fr>
2025-09-12 09:09:39 +0200

    fix: changes update.ps1 ideamaker (2)

--- automatic/ideamaker/update.ps1
+++ automatic/ideamaker/update.ps1
@@ -18,21 +18,29 @@ function global:au_BeforeUpdate {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing -UserAgent "Chocolatey" -Headers @{
-        "Accept" = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" -Headers @{
+        "Accept"                    = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+        "Accept-Language"           = "en-US,en;q=0.5"
+        "Accept-Encoding"           = "gzip, deflate"
+        "DNT"                       = "1"
+        "Connection"                = "keep-alive"
+        "Upgrade-Insecure-Requests" = "1"
     }
 
-    $url = $download_page.links | Where-Object href -Match 'install_ideaMaker_(?<version>[\d\.]+).exe$' | Select-Object -First 1 -ExpandProperty href
-    $version = $Matches.version
-
+    $release_links = $download_page.links | Where-Object href -Match 'install_ideaMaker_(?<version>[\d\.]+)\.exe$' | Where-Object href -NotMatch 'beta'
 
     return @{
         Version = $version
         URL32   = $url
         Options = @{
             Headers = @{
-                "Accept"     = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
-                "User-Agent" = "Chocolatey"
+                "Accept"                    = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+                "Accept-Language"           = "en-US,en;q=0.5"
+                "Accept-Encoding"           = "gzip, deflate"
+                "DNT"                       = "1"
+                "Connection"                = "keep-alive"
+                "Upgrade-Insecure-Requests" = "1"
+                "User-Agent"                = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
             }
         }
     }
