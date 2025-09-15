ea47d0c463ad0298867399bccc470c9e8b1db275
ROGIER Nicolas <nicolas.rogier@univ-lorraine.fr>
2025-09-15 10:35:32 +0200

    fix: checksums gretl

--- automatic/gretl/tools/chocolateyinstall.ps1
+++ automatic/gretl/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$url = 'http://prdownloads.sourceforge.net/gretl/gretl-2025b-32.exe'
-$url64 = 'http://prdownloads.sourceforge.net/gretl/gretl-2025b-64.exe'
+$url = 'http://netix.dl.sourceforge.net/project/gretl/gretl/2025b/gretl-2025b-32.exe?viasf=1'
+$url64 = 'http://deac-riga.dl.sourceforge.net/project/gretl/gretl/2025b/gretl-2025b-64.exe?viasf=1'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
@@ -9,9 +9,9 @@ $packageArgs = @{
   url            = $url
   url64bit       = $url64
   softwareName   = 'gretl*'
-  checksum       = '332ba87d79af116242e9e44a6a8ccefed9cdad2a57755aa774d5f3198120179a'
+  checksum       = '383dc045cf5ac0309beef82d1e70e476587a42ad3bd1d8923d85970d37da7b2e'
   checksumType   = 'sha256'
-  checksum64     = 'c128dd29e395e05a11fcb05da130186e71998d8f5e83ec5ac0464fe2de511715'
+  checksum64     = '01d0ff70f00e57c1d4045d0eb0bdf98ad524755db2e561a8bef7dbc0c3aee629'
   checksumType64 = 'sha256'
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
