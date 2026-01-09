bd969b91e1131f09997ee3934234c41b30b830af
MAYER Maxime <maxime.mayer@univ-lorraine.fr>
2026-01-09 09:03:52 +0100

    update ocenaudio ocenaudio.install 3.17.0

--- automatic/ocenaudio.install/tools/chocolateyinstall.ps1
+++ automatic/ocenaudio.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
     
   url64          = 'https://www.ocenaudio.com/downloads/index.php/ocenaudio_windows64.exe'
-  checksum64     = 'd4d588a33410439e12683b54c5c468768b5798a4800029e63b7e0bbf5c15e678'
+  checksum64     = '699f734ef8f6de081afee731b5128f599782094347110393c5a548a0b596cfc3'
   checksumType64 = 'sha256'
   silentArgs     = "/allusers /S"
 }
