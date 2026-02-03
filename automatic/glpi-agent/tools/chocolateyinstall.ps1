e544ae126fb3d08babd964fcb202bce4e34a5dc2
overag3 <nicolas.rogier@univ-lorraine.fr>
2026-02-03 01:21:22 +0000

    AU: 1 updated - glpi-agent [skip ci]

--- automatic/glpi-agent/tools/chocolateyinstall.ps1
+++ automatic/glpi-agent/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'msi'
-  url64         = 'https://github.com/glpi-project/glpi-agent/releases/download/1.15/GLPI-Agent-1.15-x64.msi'
-  checksum64    = '7a5c1ed8a53ac3b85c80d25c9105030354c3fd6988793d20841a216ba2c965bb'
+  url64         = 'https://github.com/glpi-project/glpi-agent/releases/download/1.16/GLPI-Agent-1.16-x64.msi'
+  checksum64    = '1210c10dfc9921f28f591a3352d8f6ef898c737980db833773ff8cda5681be90'
   checksumType64= 'sha256' 
   silentArgs    = '/quiet'
   validExitCodes= @(0, 3010, 1641)
