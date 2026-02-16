c74976d8bac20e5cabf30a30bf6ad8f06dfee7f4
overag3 <nicolas.rogier@univ-lorraine.fr>
2026-02-16 01:21:34 +0000

    AU: 1 updated - opencode-desktop [skip ci]

--- automatic/opencode-desktop/tools/chocolateyinstall.ps1
+++ automatic/opencode-desktop/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url = 'https://github.com/anomalyco/opencode/releases/download/v1.2.3/opencode-desktop-windows-x64.exe'
+$url = 'https://github.com/anomalyco/opencode/releases/download/v1.2.5/opencode-desktop-windows-x64.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'EXE'
   url64bit       = $url
   softwareName   = 'OpenCode*'
-  checksum64     = '651d698f1e44bab97648b38d5bf61e6d3d3397d5e9d982bd2eb5087537c76399'
+  checksum64     = 'ae24febfd93258eecfd7edee4d0851097a5a0fa07e6d9ea28dadfd6ccae4d4cb'
   checksumType64 = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
