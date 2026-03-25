e6fcf29d84a9faa1ba5eec25e091710c75633f66
overag3 <nicolas.rogier@univ-lorraine.fr>
2026-03-25 01:23:07 +0000

    AU: 2 updated - gmsh opencode-desktop [skip ci]

--- automatic/gmsh/tools/chocolateyinstall.ps1
+++ automatic/gmsh/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop'
-$url64 = 'https://gmsh.info/bin/Windows/gmsh-4.15.1-Windows64.zip'
+$url64 = 'https://gmsh.info/bin/Windows/gmsh-4.15.2-Windows64.zip'
 $installDir = 'C:\gmsh'
-$version = '4.15.1'
+$version = '4.15.2'
 $dirToExtract = "gmsh-$version-Windows64"
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $installDir
   url64bit       = $url64
-  checksum64     = '8deb2b9c14a4dfdb143f7c7a6f2b693da96ab8cb43445553f34156d499e4adf5'
+  checksum64     = 'a8208b56aedee3434907a893e303422acacd9003140766e09f2abbc664ba7157'
   checksumType64 = 'sha256'
 }
 
--- automatic/opencode-desktop/tools/chocolateyinstall.ps1
+++ automatic/opencode-desktop/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url = 'https://github.com/anomalyco/opencode/releases/download/v1.3.0/opencode-desktop-windows-x64.exe'
+$url = 'https://github.com/anomalyco/opencode/releases/download/v1.3.2/opencode-desktop-windows-x64.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'EXE'
   url64bit       = $url
   softwareName   = 'OpenCode*'
-  checksum64     = '973368fd4b108833d4da5d9a63603128f72d85e7e52e5c78c9e275d08bf9c110'
+  checksum64     = 'bf855e87832777597dda599bd79ec04ad460c1f8e43b701312408cb13365f257'
   checksumType64 = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
