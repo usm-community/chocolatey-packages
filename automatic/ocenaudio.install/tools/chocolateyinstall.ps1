0f5b713cb9d2e1df86611b0ef298e463204c95df
overag3 <nicolas.rogier@univ-lorraine.fr>
2026-04-17 01:52:35 +0000

    AU: 2 updated - ocenaudio.install opencode-desktop [skip ci]

--- automatic/ocenaudio.install/tools/chocolateyinstall.ps1
+++ automatic/ocenaudio.install/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   url64          = 'https://www.ocenaudio.com/downloads/index.php/ocenaudio_windows64.exe'
-  checksum64     = 'A3784F5D95129CD238DA5063619F65319D3D81AB30FD008FF3B3BBCF0CD405EE'
+  checksum64     = '59A94D1424DAFEE7138B468D90AEFE9E86CEE954487772721F08C396A9E698F5'
   checksumType64 = 'sha256'
   silentArgs     = "/allusers /S"
 }
--- automatic/opencode-desktop/tools/chocolateyinstall.ps1
+++ automatic/opencode-desktop/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url = 'https://github.com/anomalyco/opencode/releases/download/v1.4.6/opencode-desktop-windows-x64.exe'
+$url = 'https://github.com/anomalyco/opencode/releases/download/v1.4.7/opencode-desktop-windows-x64.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'EXE'
   url64bit       = $url
   softwareName   = 'OpenCode*'
-  checksum64     = '94c66d7c5fc0b5319009f683e1b24e140e814af6a843b5c04f9fbd3fd9167e35'
+  checksum64     = '498f9562270ef1e9f8db6cebe9ac9de75172dcb330d24b90d7b5751954c8f54c'
   checksumType64 = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
