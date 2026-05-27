7611be9d88cdd7e22d1201b7f096e68f8824a83e
overag3 <nicolas.rogier@univ-lorraine.fr>
2026-05-27 02:39:17 +0000

    AU: 1 updated - arduino.portable [skip ci]

--- automatic/arduino.portable/tools/chocolateyinstall.ps1
+++ automatic/arduino.portable/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$url64 = 'https://github.com/arduino/arduino-ide/releases/download/2.3.8/arduino-ide_2.3.8_Windows_64bit.zip'
-$version = '2.3.8'
+$url64 = 'https://github.com/arduino/arduino-ide/releases/download/2.3.9/arduino-ide_2.3.9_Windows_64bit.zip'
+$version = '2.3.9'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = "$($env:ProgramFiles)\Arduino IDE"
   url64          = $url64
-  checksum64     = '17879b5550b659b46cfe6b788d2f15428246c24e7dabed4826bb225cfeb27665'
+  checksum64     = 'be500101da67dfe1c21e2f722bb485a22e7d91bace7f5bb7a91737718a9a52ca'
   checksumType64 = 'sha256'
 }
 
