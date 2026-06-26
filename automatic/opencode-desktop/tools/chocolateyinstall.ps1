3cb8407971b7905202bbc7740e076b557214ed3f
overag3 <nicolas.rogier@univ-lorraine.fr>
2026-06-26 02:36:00 +0000

    AU: 2 updated - opencode-desktop rancher-desktop [skip ci]

--- automatic/opencode-desktop/tools/chocolateyinstall.ps1
+++ automatic/opencode-desktop/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url = 'https://github.com/anomalyco/opencode/releases/download/v1.17.10/opencode-desktop-win-x64.exe'
+$url = 'https://github.com/anomalyco/opencode/releases/download/v1.17.11/opencode-desktop-win-x64.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'EXE'
   url64bit       = $url
   softwareName   = 'OpenCode*'
-  checksum64     = 'dde3c0bb9e45653728b3c25421a7e8bb0de7e14aec6fafd04c7093f232f3d5c9'
+  checksum64     = 'ee89e509f12db7b5d7c9c508bf762bd592d772a231e67014dd59ed6a3e835a61'
   checksumType64 = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
--- automatic/rancher-desktop/tools/chocolateyinstall.ps1
+++ automatic/rancher-desktop/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop'
-$url = 'https://github.com/rancher-sandbox/rancher-desktop/releases/download/v1.22.3/Rancher.Desktop.Setup.1.22.3.msi'
+$url = 'https://github.com/rancher-sandbox/rancher-desktop/releases/download/v1.23.0/Rancher.Desktop.Setup.1.23.0.msi'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'MSI'
   url            = $url
   softwareName   = 'Rancher Desktop*'
-  checksum       = 'd0918c3e99d2918d84d739e47df12964b2d75ccdf3f9da769b6d4b47ede187cf'
+  checksum       = '750d2e12666336e4abd875a2c9cb80d7471b907b139616400de2a5cfdf6cff35'
   checksumType   = 'sha256'
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0, 3010, 1641)
