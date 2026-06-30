38bbbb52f4b984090d9e7a7897814355b46a73de
overag3 <nicolas.rogier@univ-lorraine.fr>
2026-06-30 02:35:58 +0000

    AU: 1 updated - rancher-desktop [skip ci]

--- automatic/rancher-desktop/tools/chocolateyinstall.ps1
+++ automatic/rancher-desktop/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop'
-$url = 'https://github.com/rancher-sandbox/rancher-desktop/releases/download/v1.23.0/Rancher.Desktop.Setup.1.23.0.msi'
+$url = 'https://github.com/rancher-sandbox/rancher-desktop/releases/download/v1.23.1/Rancher.Desktop.Setup.1.23.1.msi'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'MSI'
   url            = $url
   softwareName   = 'Rancher Desktop*'
-  checksum       = '750d2e12666336e4abd875a2c9cb80d7471b907b139616400de2a5cfdf6cff35'
+  checksum       = '65c2daa6e7d6b5dbd172d51f9eb72d4a318869deb722a99decc82e87ae068102'
   checksumType   = 'sha256'
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0, 3010, 1641)
