2e085a827d228ca7cc858355ab82fb49f88bc450
overag3 <nicolas.rogier@univ-lorraine.fr>
2026-07-14 01:44:45 +0000

    AU: 2 updated - freefemplusplus opencode-desktop [skip ci]

--- automatic/freefemplusplus/tools/chocolateyinstall.ps1
+++ automatic/freefemplusplus/tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop'
-$url64 = 'https://github.com/FreeFem/FreeFem-sources/releases/download/v4.16/FreeFEM-4.16-amd64-win64.exe'
+$url64 = 'https://github.com/FreeFem/FreeFem-sources/releases/download/v4.17/FreeFEM-4.17-amd64-win64.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
@@ -7,7 +7,7 @@ $packageArgs = @{
   fileType       = 'EXE'
   url64bit       = $url64
   softwareName   = 'freefem*'
-  checksum64     = '38df81255bbff05e12dc88142dae5ca01cc20d8654edcf0530a82fff81268f95'
+  checksum64     = 'f78737f7fb3b46961588ff9028e9241c76804414c4425511a776ece3274f5d7a'
   checksumType64 = 'sha256'
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
--- automatic/opencode-desktop/tools/chocolateyinstall.ps1
+++ automatic/opencode-desktop/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url = 'https://github.com/anomalyco/opencode/releases/download/v1.17.18/opencode-desktop-win-x64.exe'
+$url = 'https://github.com/anomalyco/opencode/releases/download/v1.17.20/opencode-desktop-win-x64.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'EXE'
   url64bit       = $url
   softwareName   = 'OpenCode*'
-  checksum64     = '8e1095316dd7ac150322002caa719cd5c7f83b11018f8036e1e948485dfad6f8'
+  checksum64     = '7cc70f63d21656714333123648db0fb9cf9bf0a71a095b53fc67d204d61d3cdf'
   checksumType64 = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
