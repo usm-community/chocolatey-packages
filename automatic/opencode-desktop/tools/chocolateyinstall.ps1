db201da21e4f2a276546a4cf514a807fc7ed438f
overag3 <nicolas.rogier@univ-lorraine.fr>
2026-07-25 01:54:02 +0000

    AU: 2 updated - ocenaudio opencode-desktop [skip ci]

--- automatic/opencode-desktop/tools/chocolateyinstall.ps1
+++ automatic/opencode-desktop/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url = 'https://github.com/anomalyco/opencode/releases/download/v1.18.4/opencode-desktop-win-x64.exe'
+$url = 'https://github.com/anomalyco/opencode/releases/download/v1.18.5/opencode-desktop-win-x64.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'EXE'
   url64bit       = $url
   softwareName   = 'OpenCode*'
-  checksum64     = '5cc5fedc58811bd797f1aec1c789e0772a0be430c923dadf9f09c24181435492'
+  checksum64     = '198db6ea0b8e93935b7912df9a1e5a0239711da7d2954a7338da51b920d51ef9'
   checksumType64 = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
