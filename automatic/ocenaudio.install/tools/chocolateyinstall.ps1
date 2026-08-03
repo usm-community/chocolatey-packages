f2e4e78d20551fd482a3046612635f5c2e95c096
overag3 <nicolas.rogier@univ-lorraine.fr>
2026-08-03 01:59:47 +0000

    AU: 1 updated - ocenaudio.install [skip ci]

--- automatic/ocenaudio.install/tools/chocolateyinstall.ps1
+++ automatic/ocenaudio.install/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   url64          = 'https://www.ocenaudio.com/downloads/index.php/ocenaudio_windows64.exe'
-  checksum64     = 'C9A31C8FD00C8D5E2B0417C1AEA105B72BB095A87A4433574805C16BFC98D8BE'
+  checksum64     = 'C4322ADBCE034F9E90A576B353D48ED46056552EAAA76B2703D08DA5C4E0E037'
   checksumType64 = 'sha256'
   silentArgs     = "/allusers /S"
 }
