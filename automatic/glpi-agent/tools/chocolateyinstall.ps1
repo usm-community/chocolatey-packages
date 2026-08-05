732efe41d3f81eb282ec054e38551e992e71b388
overag3 <nicolas.rogier@univ-lorraine.fr>
2026-08-05 01:47:57 +0000

    AU: 2 updated - glpi-agent opencode-desktop [skip ci]

--- automatic/glpi-agent/tools/chocolateyinstall.ps1
+++ automatic/glpi-agent/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'msi'
-  url64         = 'https://github.com/glpi-project/glpi-agent/releases/download/1.18/GLPI-Agent-1.18-x64.msi'
-  checksum64    = '31f60fe349c56f4eac1d4d092441efa770924714b3fb61f18e4915e3dd70f9c4'
+  url64         = 'https://github.com/glpi-project/glpi-agent/releases/download/1.19/GLPI-Agent-1.19-x64.msi'
+  checksum64    = 'f3f933a54bc325ffe0d6063e177874e05138dd887fe690adef337640e8d6335c'
   checksumType64= 'sha256' 
   silentArgs    = '/quiet'
   validExitCodes= @(0, 3010, 1641)
--- automatic/opencode-desktop/tools/chocolateyinstall.ps1
+++ automatic/opencode-desktop/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url = 'https://github.com/anomalyco/opencode/releases/download/v1.18.12/opencode-desktop-win-x64.exe'
+$url = 'https://github.com/anomalyco/opencode/releases/download/v1.18.13/opencode-desktop-win-x64.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'EXE'
   url64bit       = $url
   softwareName   = 'OpenCode*'
-  checksum64     = '2ba83a1105d756532a91491b726978daa007d53d54ee2629d52074b746d55334'
+  checksum64     = '3d1796daa0762ec49cdb27f8418b8e0d2dafb37d89dd4ea766b42bdd7cd6d260'
   checksumType64 = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
