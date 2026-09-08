8a580fff0e2b3444995ad3f686e7c85ed7241f08
Nicolas Rogier <nicolas.rogier@univ-lorraine.fr>
2026-09-08 11:57:57 +0200

    fix: update URL handling and improve checksum retrieval in updater script

--- automatic/purplepen/update.ps1
+++ automatic/purplepen/update.ps1
@@ -2,24 +2,24 @@ param([string]$IncludeStream, [switch]$Force)
 
 Import-Module Chocolatey-AU
 
-$releases = 'https://purple-pen.org/downloads'
+$releases  = 'https://purple-pen.org/downloads'
+$userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
 
 function global:au_SearchReplace {
     @{
         'tools\chocolateyinstall.ps1' = @{
-            "(^[$]url\s*=\s*)('.*')"          = "`$1'$($Latest.URL)'"
+            "(^[$]url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
             "(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
         }
     }
 }
 
 function global:au_BeforeUpdate {
-    Get-RemoteFiles -Purge -NoSuffix
-    $Latest.Checksum32 = Get-RemoteChecksum $Latest.URL32
+    $Latest.Checksum32 = Get-RemoteChecksum $Latest.URL32 -Headers $Latest.Options.Headers
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing -UserAgent $userAgent
     $regex = 'purplepen-(\d)(\d)(\d)\.exe$'
 
     $url = ($download_page.Links | Where-Object href -Match $regex | Select-Object -Last 1 -ExpandProperty href)
@@ -27,7 +27,13 @@ function global:au_GetLatest {
 
     return @{
         Version = $version
-        URL     = "$releases/$url"
+        URL32   = "$releases/$url"
+        Options = @{
+            Headers = @{
+                'User-Agent' = $userAgent
+                'Referer'    = "$releases/"
+            }
+        }
     }
 
 }
