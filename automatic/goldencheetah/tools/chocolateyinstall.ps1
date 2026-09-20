a2f0b535dd6366496bc4c196c4492675be433ff3
overag3 <nicolas.rogier@univ-lorraine.fr>
2026-09-20 02:35:31 +0000

    AU: 1 updated - goldencheetah [skip ci]

--- automatic/goldencheetah/tools/chocolateyinstall.ps1
+++ automatic/goldencheetah/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop'
-$url64 = 'https://github.com/GoldenCheetah/GoldenCheetah/releases/download/v3.7-SP1/GoldenCheetah_v3.7-sp1_x64Qt6.exe'
+$url64 = 'https://github.com/GoldenCheetah/GoldenCheetah/releases/download/v3.8/GoldenCheetah_v3.8_x64.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'EXE'
   url64bit       = $url64
   softwareName   = 'goldencheetah*'
-  checksum64     = 'e00d3bcea114c54b64f270b9ab467d1513207d296de85cae2bb1ac2ae25a355f'
+  checksum64     = '7aae10c26f67698a690505ec0b3011f7c8e6b7a12cd5026f8a175579b4598c58'
   checksumType64 = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
